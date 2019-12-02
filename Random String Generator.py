28717e7983fba7246ebac33d51ab34323f39c5a2
Niharika-Arora <aroraniharika15@gmail.com>
2019-12-02 23:42:28 +0530

    text file added

--- Random String Generator.py
+++ Random String Generator.py
@@ -1,7 +1,7 @@
 import random
 
 
-file1 = open(r"C:\Users\Neha\Desktop\random movies.txt", "r+")
+file1 = open(r"C:\Users\Neha\PycharmProjects\personal projects\random movies.txt", "r+")
 line = random.choice(list(file1))
 print(line)
 file1.close()
