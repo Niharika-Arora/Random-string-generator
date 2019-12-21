f1f287ef29e230603f520024fe802ab3d8e7d3db
Psycho Mods <nishsin26@gmail.com>
2019-12-22 00:13:22 +0530

    Bringup reset method to reset the available file to initial state

--- RandomStringGenerator.py
+++ RandomStringGenerator.py
@@ -1,9 +1,10 @@
 import random
 import sys
+import os
 
 
 def random_name():
-    file1 = open(r"movies_available.txt", "r+")
+    file1 = open(r"movies_available.txt", "r")
     line = random.choice(list(file1))
     file1.close()
     return line
@@ -12,7 +13,7 @@ def random_name():
 def menu():
     print("*********Welcome to the world of randoms********")
     print("What would you like to do?")
-    print("1. Get a new name \n2. To exit")
+    print("1. Get a new name \n2. To exit \n3. To reset the available movies list")
     ch = int(input("Enter the corresponding number for your choice : "))
     return ch
 
@@ -38,6 +39,17 @@ def shift(s):
     done.close()
 
 
+def reset():
+    available = open(r"movies_available.txt", "a")
+    used = open(r"movies_used.txt", "r")
+    for movie in list(used):
+        available.write(movie)
+    used.close()
+    os.remove(r"movies_used.txt")
+    available.close()
+    print("Reset successful")
+
+
 if __name__ == "__main__":
     while True:
         choice = menu()
@@ -47,12 +59,21 @@ if __name__ == "__main__":
                 print(name)
                 print("Enter 'y' to accept\nEnter 'n' to choose another name\nEnter 'e' to go back to main menu")
                 choice1 = input("So which is it? : ")
-                if choice1 == 'y' or choice1 == 'Y':
+                if choice1.casefold() == 'y'.casefold():
                     shift(name)
                     break
-                if choice1 == 'e' or choice1 == 'E':
+                if choice1.casefold() == 'e'.casefold():
                     break
                 else:
                     continue
-        else:
+        elif choice == 2:
             sys.exit(0)
+        elif choice == 3:
+            print("This will reset the available movies to initial unselected state")
+            choice1 = input("Continue with reset? (y/n) : ")
+            if choice1.casefold() == 'y'.casefold():
+                reset()
+            else:
+                break
+        else:
+            print("Wrong choice, Please try again.")
